dd8974ed46c6209d29a38a973ac75cd39546153e
Suleymanov Andrey <sasus37@gmail.com>
2022-07-09 13:05:51 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+void evendigits ( long long number, int &ans){
+    if ( (number%10)%2 == 0) {ans++;}
+if (number > 10){
+    evendigits ( number/10, ans);
+}
 
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int ans = 0;
+
+    evendigits (9223372036854775806, ans);
+
+    std::cout << ans;
 }
